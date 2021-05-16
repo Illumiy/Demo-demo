f6f5410281136ae4d71a0d23524faf80291ce882
Illumiy <dima.skobelev.1979@mail.ru>
2021-05-16 14:07:20 +0300

    final bd

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=wsr',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=wsr',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
